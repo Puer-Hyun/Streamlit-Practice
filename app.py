cd6eef8eaf408b9bb421a88c404fb6e171275050
SeongHyun <silkcoop@gmail.com>
2023-04-27 01:38:32 +0900

    Delete old and wrong model file and redirect files

--- app.py
+++ app.py
@@ -15,24 +15,25 @@ def main():
     st.title("Book Rating Prediction using CatBoost")
     model = catboost.CatBoostRegressor()
     #기존에 저장해 둔 모델만을 불러오는 것이기 때문에, Batch service의 개념과 같다.
-    model.load_model('20230426_093711')
+    model.load_model('/Users/seonghyunpark/python_boostcamp2/project-serving/trained_model_20230426_133927.cbm')
 
     uploaded_file = st.file_uploader("Upload your pickle file", type=['pkl'])
 
-    if uploaded_file : 
-        _data = uploaded_file.getvalue()
-        print(_data)
+    # if uploaded_file : 
+    #     _data = uploaded_file.getvalue()
+    #     print(_data)
 
+main()
 
-@cache_on_button_press('Authenticate')
-def authenticate(password, userid) ->bool:
-    return (password == root_password) & (userid == root_userid)
+# @cache_on_button_press('Authenticate')
+# def authenticate(password, userid) ->bool:
+#     return (password == root_password) & (userid == root_userid)
 
-userid = st.text_input('userid', type="password")
-password = st.text_input('password', type="password")
+# userid = st.text_input('userid', type="password")
+# password = st.text_input('password', type="password")
 
-if authenticate(password, userid):
-    st.success('You are authenticated!')
-    main()
-else:
-    st.error('The password is invalid.')
+# if authenticate(password, userid):
+#     st.success('You are authenticated!')
+#     main()
+# else:
+#     st.error('The password is invalid.')
