0bef13185cd71c12e7abb5003328acd02a21b998
SeongHyun <silkcoop@gmail.com>
2023-04-26 21:34:41 +0900

    Add catboost model, train, test data

--- Mission_practice/app.py
+++ Mission_practice/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from confirm_button_hack import cache_on_button_press
+import catboost
 st.write()
 
 # SETTING PAGE CONFIG TO WIDE MODE
@@ -11,8 +12,16 @@ root_userid = 'Seonghyun'
 #TODO 모델을 학습하기 위한 데이터도 함께 불러오기 
 
 def main():
-    st.title("Book Rating Prediction")
+    st.title("Book Rating Prediction using CatBoost")
+    model = catboost.CatBoostRegressor()
+    #기존에 저장해 둔 모델만을 불러오는 것이기 때문에, Batch service의 개념과 같다.
+    model.load_model('20230426_093711')
 
+    uploaded_file = st.file_uploader("Upload your pickle file", type=['pkl'])
+
+    if uploaded_file : 
+        _data = uploaded_file.getvalue()
+        print(_data)
 
 
 @cache_on_button_press('Authenticate')
